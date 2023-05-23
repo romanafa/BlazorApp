1b3b26a721d7fe386bcdf6761cf797922db6d716
Romana Faiglova <r.faiglova@gmail.com>
2023-05-23 12:04:05 +0200

    Added Category

---
+++ BlazorApp_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,79 @@
+﻿using BlazorApp_Business.Repository.IRepository;
+using BlazorApp_DataAccess.Data;
+using BlazorApp_DataAccess;
+using BlazorApp_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BlazorApp_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public CategoryDto Create(CategoryDto categoryDto)
+        {
+            var category = _mapper.Map<CategoryDto, Category>(categoryDto);
+            category.CreatedDate = DateTime.Now;
+
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+
+            return _mapper.Map<Category, CategoryDto>(category);
+        }
+
+        public int Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if(category != null)
+            {
+                _db.Categories.Remove(category);
+                return _db.SaveChanges();
+            }
+            return 0;
+
+        }
+
+        public IEnumerable<CategoryDto> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDto>>(_db.Categories);
+        }
+
+        public CategoryDto GetById(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                return _mapper.Map<Category, CategoryDto>(category);
+            }
+            return new CategoryDto();
+        }
+
+        public CategoryDto Update(CategoryDto categoryDto)
+        {
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == categoryDto.Id);
+
+            if(categoryFromDb != null)
+            {
+                categoryFromDb.Name = categoryDto.Name;
+                _db.Categories.Update(categoryFromDb);
+                _db.SaveChanges();
+                return _mapper.Map<Category, CategoryDto>(categoryFromDb);
+            }
+
+            return categoryDto;
+
+        }
+    }
+}
---
+++ BlazorApp_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDto Create(CategoryDto categoryDto);
+        public CategoryDto Update(CategoryDto categoryDto);
+        public int Delete(int id);
+        public CategoryDto GetById(int id);
+        public IEnumerable<CategoryDto> GetAll();
+    }
+}
