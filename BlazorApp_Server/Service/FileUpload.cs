52a42c9f71fbad54247fcb5af40d200b7369f7bf
Romana Faiglova <r.faiglova@gmail.com>
2023-05-25 11:02:50 +0200

    File upload service

---
+++ BlazorApp_Server/Service/FileUpload.cs
@@ -0,0 +1,45 @@
+﻿using BlazorApp_Server.Service.IService;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorApp_Server.Service
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public bool DeleteFile(string filePath)
+        {
+            if(File.Exists(_webHostEnvironment.WebRootPath+filePath))
+            {
+                File.Delete(_webHostEnvironment.WebRootPath+filePath);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            FileInfo fileInfo = new FileInfo(file.Name);
+            var fileName = Guid.NewGuid().ToString()+fileInfo.Extension;
+            var folderDirectory = $"{_webHostEnvironment.WebRootPath}\\images\\product";
+
+            // check if the directory exists
+            if(!Directory.Exists(folderDirectory))
+            {
+                Directory.CreateDirectory(folderDirectory);
+            }
+
+            var filePath = Path.Combine(folderDirectory, fileName);
+
+            await using FileStream fileStream = new FileStream(filePath, FileMode.Create);
+            await file.OpenReadStream().CopyToAsync(fileStream);
+
+            var fullPath = $"/images/product/{fileName}";
+            return fullPath;
+        }
+    }
+}
